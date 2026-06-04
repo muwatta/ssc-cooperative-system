b2c0d80c8df87d07d297a8aed8bea77cc2676a7d
Muwatta <abdullahmusliudeen@gmail.com>
2026-06-04 09:35:36 +0100

    enable Vercel Speed Insights

--- ssc_frontend/src/main.tsx
+++ ssc_frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { AuthProvider } from "@/context/AuthContext";
 import AppRouter from "@/routes";
 import "@/index.css";
@@ -21,6 +22,7 @@ createRoot(document.getElementById("root")!).render(
       <AuthProvider>
         <AppRouter />
         <Analytics />
+        <SpeedInsights />
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
